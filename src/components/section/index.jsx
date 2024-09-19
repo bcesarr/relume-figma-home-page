0c1d020bd61cf534055797c1e209a136f798c745
bcesar7 <brunocesar.as7@gmail.com>
2024-09-19 03:06:17 -0300

    atu: seções adicionadas

--- src/components/section/index.jsx
+++ src/components/section/index.jsx
@@ -1,17 +1,17 @@
 import "./style.css";
 import Image from "../../assets/images/placeholder-image.svg";
 
-export default function Section({ aligment }) {
+export default function Section({ title, subtitle, paragraph, aligment }) {
     return (
         <div className={`section-container section-${aligment}`}>
             <img src={Image} alt="Imagem de local de imagem vazio para se colocar imagem" />
 
             <div className="section-text">
-                <h6>Feature three</h6>
-
-                <h2>Describe benefit of feature three</h2>
-
-                <p>Highlight Unique Selling Proposition (USP) with a short summary of the key feature and how it benefits customers.</p>
+                <h6>{subtitle}</h6>
+                
+                <h2>{title}</h2>
+                
+                <p>{paragraph}</p>
 
                 <button>Learn more</button>
 
