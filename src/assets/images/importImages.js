bdca56a1400b66871a55cdc5ef1a44aada0e5aed
bcesar7 <brunocesar.as7@gmail.com>
2024-09-22 00:09:24 -0300

    atu: adicionando arquivo para exportação das imagens

---
+++ src/assets/images/importImages.js
@@ -0,0 +1,17 @@
+// Exportando imagens
+export { default as CompanyLogo } from './company-logo.svg';
+export { default as PlaceholderImage} from './placeholder-image.svg';
+export { default as AvatarImage} from './avatar-image.svg';
+export { default as WebflowLogo} from './webflow-logo.svg';
+export { default as RelumeLogo } from './relume-logo.svg';
+
+// Exportando icones
+export { default as IconCube } from './icons/icon-cube.svg';
+export { default as IconDivider} from './icons/icon-divider.svg';
+export { default as LogoFacebook} from './icons/icon-facebook.svg';
+export { default as LogoInstagram } from './icons/icon-instagram.svg';
+export { default as LogoLinkedin } from './icons/icon-linkedin.svg';
+export { default as IconMobileMenu } from './icons/icon-mobile-menu.svg';
+export { default as IconStars } from './icons/icon-stars.svg';
+export { default as LogoX } from './icons/icon-x.svg';
+export { default as LogoYoutube } from './icons/icon-youtube.svg';
